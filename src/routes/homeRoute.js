4d2ddbc3435454fae71219ad8428dac4f1ca55e8
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-03-15 01:17:32 +0900

    [Feat] 인기 코스 화면 라우터 추가

--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -37,4 +37,33 @@ homeRouter.get('/', async (req, res) => {
     }
 })
 
+/* 인기코스 화면 */
+homeRouter.get('/populars', async (req, res) => {
+    try {
+        const { isSeoul } = req.query
+        let courses;
+        
+        if(isSeoul === 'true') {
+            courses = await Course.find({ isSeoul: true }).limit(100)
+        } else {
+            courses = await Course.find({ isSeoul: false }).limit(100)
+        }
+
+        return res.status(200).json({
+            status: 200,
+            success: true,
+            message: "인기 코스 화면 조회 성공",
+            data: {
+                course: courses
+            }
+        });
+    } catch (err) {
+        return res.status(500).json({  
+            status: 500,
+            success: false,
+            message: "서버 내부 에러"
+        });
+    }
+})
+
 module.exports = { homeRouter }

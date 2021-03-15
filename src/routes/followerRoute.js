bcf9570e5dcf617fddadf42abb93e7073bf85c5a
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-03-15 22:13:03 +0900

    [Feat] 언팔로우 라우터 추가

--- src/routes/followerRoute.js
+++ src/routes/followerRoute.js
@@ -92,4 +92,42 @@ followerRouter.post('/:userId/follow', async (req, res) => {
     }
 })
 
+/* 언팔로우 */
+followerRouter.post('/:userId/unfollow', async (req, res) => {
+    try {
+        const { userId } = req.params
+        const { userFrom } = req.body
+        const [ userToId, userFromId ] = await Promise.all([
+            User.findById(userId, {}, {  }),
+            User.findById(userFrom, {}, {  })
+        ])
+        if(!userToId || !userFromId) 
+                return res.status(400).json({ 
+                    status: 400,
+                    success: false,
+                    message: "존재하지 않는 유저입니다."
+                 });
+
+        await Follower.findOneAndDelete({ userTo: userId, userFrom: userFrom })
+            .exec((err, doc) => {
+                if(err) return res.status(400).json({ 
+                    status: 400,
+                    success: false,
+                    message: "팔로우 삭제를 실패하였습니다.", 
+                })
+                return res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: "언팔로우 성공"
+                })
+        })
+    } catch (err) {
+        return res.status(500).json({  
+            status: 500,
+            success: false,
+            message: "서버 내부 에러"
+        });
+    }
+})
+
 module.exports = { followerRouter }
